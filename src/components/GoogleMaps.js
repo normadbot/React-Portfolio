7910adb135d693e89b447f46596cd6bbabee3121
Sarabjeet <sabysingh298@gmail.com>
2021-05-17 21:06:20 +0530

    Map changes

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -4,13 +4,9 @@ import DeckGL from "@deck.gl/react";
 import { StaticMap } from "react-map-gl";
 import { IconLayer } from "@deck.gl/layers";
 
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1Ijoic2FieXNpbmdoMjk4IiwiYSI6ImNrb2xybDk3bDAyY3Yybm80bnplaXg5dnAifQ.W1ruChOZ_LrlxvH-8J6VbA";
 
-  const Map = ReactMapboxGl({
-    accessToken:
-      'pk.eyJ1Ijoic2FieXNpbmdoMjk4IiwiYSI6ImNrb2xybDk3bDAyY3Yybm80bnplaXg5dnAifQ.W1ruChOZ_LrlxvH-8J6VbA'
-  });
 const INITIAL_VIEW_STATE = {
   longitude: 77.594566,
   latitude: 12.971599,
@@ -20,33 +16,45 @@ const INITIAL_VIEW_STATE = {
 };
 
 const ICON__MAPPING = {
-  marker: { x: 0, y: 0, width: 128, height: 128, mask: false },
-};
+    marker: { x: 0, y: 0, width: 128, height: 128, mask: false }
+  };
 
 function GoogleMaps() {
-  
+    const iconLayer = new IconLayer({
+        id: "icon-layer",
+        data: [
+          {
+            
+            coordinates: [ 77.594566,12.971599,]
+          }
+        ],
+        pickable: true,
+        // iconAtlas and iconMapping are required
+        // getIcon: return a string
+        iconAtlas: "https://img.icons8.com/ultraviolet/40/000000/place-marker--v2.png",
+        getIcon: (d) => "marker",
+        iconMapping: ICON__MAPPING,
+        sizeScale: 15,
+        getPosition: (d) => d.coordinates,
+        getSize: (d) => 15,
+        getColor: (d) => [Math.sqrt(d.exits), 140, 0]
+      });
   return (
     <React.Fragment>
       <div id="googlemap" className="googlemap">
-        <Map
-      xs
-        zoom={[12]}
-        center={[ 77.594566,12.971599]}
-          style="mapbox://styles/mapbox/dark-v10"
-          containerStyle={{
-            height: "80%",
-            width: "100%",
-          }}
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          height="80%"
+          width="100%"
+          controller={false}
+          layers={iconLayer}
         >
-          <Layer 
-            type="symbol"
-            id="marker"
-            layout={{ "icon-image": "marker-15" }}
-          >
-            <Feature  coordinates={[ 77.594566,12.971599]} />
-          </Layer>
-        </Map>
-        ;
+          <StaticMap
+            mapStyle="mapbox://styles/mapbox/dark-v10"
+            preventStyleDiffing={true}
+            mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+          />
+        </DeckGL>
       </div>
     </React.Fragment>
   );
